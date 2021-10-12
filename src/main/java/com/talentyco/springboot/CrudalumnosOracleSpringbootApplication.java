4c5ad12778beb7c768329d909ccf3424552c2944
Emmanuel-spring <e.nieto@alumnos.santotomas.cl>
2021-10-11 21:23:43 -0300

    first

---
+++ src/main/java/com/talentyco/springboot/CrudalumnosOracleSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.talentyco.springboot.crudalumnosoraclespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudalumnosOracleSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudalumnosOracleSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talentyco/springboot/crudalumnosoraclespringboot/CrudalumnosOracleSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentyco.springboot.crudalumnosoraclespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudalumnosOracleSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
